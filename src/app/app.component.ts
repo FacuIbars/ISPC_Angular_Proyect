5b884910ddda2c1fcfcba32a9ca9165c0b75e5e8
IbarsFacundo <facundo.ibars@techforb.com>
2023-08-25 15:37:04 -0300

    estructura basica

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ISPC_Python_Proyect';
+}
