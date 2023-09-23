d599068113f2344da7f19369fe7d79c74217be2c
María Laura Escanes <129324078+EscanesMLaura@users.noreply.github.com>
2023-09-23 14:48:22 -0300

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeViewComponent } from './views/home-view/home-view.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeViewComponent,  },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,20 @@ import { TablePersonsComponent } from './components/table-persons/table-persons.
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'; 
+
+
+library.add(fas); // Agrega los iconos sólidos a la biblioteca
 @NgModule({
   declarations: [
     AppComponent,
     HomeViewComponent,
-    TablePersonsComponent
+    TablePersonsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +30,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MaterialModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    
+
+    FormsModule,
+    FontAwesomeModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
